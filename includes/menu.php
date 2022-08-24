8167565b54e746ccea8ef8f0c33b7c14ad43bdfc
paulo <paulo.simoes@aluno.ifsp.edu.br>
2022-08-24 10:01:47 -0300

    Blog 4, falta algumas coisas

---
+++ includes/menu.php
@@ -0,0 +1,21 @@
+<div class="card">
+    <div class="card-header">
+        Menu
+    </div>
+    <div class="card-body">
+        <ul class="nav-link flex-column">
+            <li class="nav-link">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-link">
+                <a class="nav-link" href="usuario_formulario.php">Cadastrre-se</a>
+            </li>
+            <li class="nav-link">
+                <a class="nav-link" href="login_formulario.php">Login</a>
+            </li>
+            <li class="nav-link">
+                <a class="nav-link" href="post_formulario.php">Incluir Post</a>
+            </li>
+        </ul>
+    </div>
+</div>
