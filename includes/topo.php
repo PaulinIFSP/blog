2b35a87f041102d2d897dd4622daaf1dfd49238f
paulo <paulo.simoes@aluno.ifsp.edu.br>
2022-09-14 12:13:12 -0300

    blog 6 e 7, mudança no topo.php

--- includes/topo.php
+++ includes/topo.php
@@ -4,11 +4,11 @@
 
 <div class="card">
     <div class="card-header">
-        <h1> Projeto Blog em PHP + MySQL IFSP - PAULO</h1>
+        <h1> Projeto Blog em PHP + MySQL IFSP - MURILO</h1>
     </div>
-    <?php if(isset($_SESSION['login']))?>
+    <?php if(isset($_SESSION['login'])): ?>
     <div class="card-body text-right">
-        Olá <?php echo $_SESSION['login']['usuario']['nome']?>
+        Olá <?php echo $_SESSION['login']['usuario']['nome']?>!
         <a href="core/usuario_repositorio.php?acao=logout"
         class="btn btn-link btn-sm" role="button">Sair</a>
     </div>
